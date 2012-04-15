47f7a7ba54b1c2933135436ef979c0954395e019
Matteo Depalo <matteodepalo@gmail.com>
2012-04-15 15:27:52 +0200

    Added better time formatting

--- client/client.js
+++ client/client.js
@@ -1,28 +1,44 @@
 Meteor.startup(function(){
   Meteor.setInterval(function() {
-    Session.set('now', now());
-  }, 0);
+    Session.set('now', Date.now().getTime());
+  }, 100);
 })
 
 Template.timers.timers = function () {
   return Timers.find({}, {sort: {expires_at: -1}});
 };
 
-Template.timer.seconds_left = function () {
-  var seconds_left = this.expires_at - Session.get('now');
-  if(seconds_left > 0) {
-    return seconds_left
+Template.timer.expires_in = function () {
+  var expires_in = (this.expires_at - Session.get('now'));
+
+  if(expires_in > 0) {
+    return to_string(expires_in);
   }
   else {
     return 0
   }
 }
 
+Template.timers.parsed_date = function () {
+  var parsed_date = Session.get('parsed_date');
+
+  if(parsed_date !== null && parsed_date !== undefined) {
+    return parsed_date
+  }
+  else {
+    return Date.now()
+  }
+}
+
 Template.timers.events = {
-  'click .submit': function () {
-    var input = parseFloat($('input.expires_at').val());
-    var name = $('input.name').val()
-    var expires_at = input + now();
+  'click input.submit': function () {
+    var expires_at = Session.get('parsed_date').getTime();
+    console.log(expires_at)
+    var name = $('input#name').val()
     Timers.insert({expires_at: expires_at, name: name});
+  },
+
+  'keyup input#date': function () {
+    Session.set('parsed_date', Date.parse($('input#date').val()))
   }
 };
--- sveglia.js
+++ sveglia.js
@@ -1,6 +1,13 @@
 Timers = new Meteor.Collection('timers');
 
-var now = function() {
-  var date = new Date();
-  return Math.floor(date.getTime()/1000);
+var time_with_zero = function (time_component) {
+  return (time_component < 10 ? ('0' + time_component) : time_component)
+}
+
+var to_string = function(time_in_milliseconds) {
+  var time_in_seconds = time_in_milliseconds/1000
+  var hour = Math.floor(time_in_seconds/60/60%24);
+  var min = Math.floor(time_in_seconds/60%60);
+  var sec = Math.floor(time_in_seconds%60);
+  return time_with_zero(hour) + ':' + time_with_zero(min) + ':' + time_with_zero(sec)
 }
